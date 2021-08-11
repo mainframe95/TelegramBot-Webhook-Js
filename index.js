8a8d86492b0b21aaf381c32eae45d31095060101
mainframe95 <46539570+mainframe95@users.noreply.github.com>
2021-08-11 10:51:50 +0000

    add menu

--- index.js
+++ index.js
@@ -1,104 +1,22 @@
-const { Telegraf } = require('telegraf')
+const {Telegraf} = require('telegraf')
+const {MenuTemplate, MenuMiddleware} = require('telegraf-inline-menu')
+// interface MyContext extends TelegrafContext {
+// 	readonly match: RegExpExecArray | undefined;
+// }
 
-const mySecret = process.env['BOT_KEY'];
-const bot = new Telegraf(`${mySecret}`);
+const menuTemplate = new MenuTemplate(ctx => `Hey ${ctx.from.first_name}!`)
 
-bot.command('start', ctx => {
-    console.log('r', ctx.from)
-    bot.telegram.sendMessage(ctx.chat.id, 'hello there! Welcome to my new telegram bot.', {
-    })
-});
-
-bot.command('hello', ctx => {
-    console.log('r', ctx.from)
-    bot.telegram.sendMessage(ctx.chat.id, 'commands hello.', {
-    })
-});
-
-//method that displays the inline keyboard buttons 
-
-bot.hears('animals', ctx => {
-    console.log(ctx.from)
-    let animalMessage = `great, here are pictures of animals you would love`;
-    ctx.deleteMessage();
-    bot.telegram.sendMessage(ctx.chat.id, animalMessage, {
-        reply_markup: {
-            inline_keyboard: [
-                [{
-                        text: "dog",
-                        callback_data: 'dog'
-                    },
-                    {
-                        text: "cat",
-                        callback_data: 'cat'
-                    }
-                ],
-
-            ]
-        }
-    })
+menuTemplate.interact('I am excited!', 'a', {
+	do: async ctx => {
+		await ctx.reply('As am I!')
+		return false
+	}
 })
 
-//method that returns image of a dog
+const bot = new Telegraf(process.env['BOT_KEY'])
 
-bot.action('dog', ctx => {
-    bot.telegram.sendPhoto(ctx.chat.id, {
-        source: "./files/dog.jpg"
-    })
+const menuMiddleware = new MenuMiddleware('/', menuTemplate)
+bot.command('start', ctx => menuMiddleware.replyToContext(ctx))
+bot.use(menuMiddleware)
 
-})
-
-//method that returns image of a cat 
-
-bot.action('cat', ctx => {
-    bot.telegram.sendPhoto(ctx.chat.id, {
-        source: "./files/cat.jpg"
-    })
-
-})
-bot.hears('phone', (ctx, next) => {
-    console.log(ctx.from)
-    bot.telegram.sendMessage(ctx.chat.id, 'Can we get access to your phone number?', requestPhoneKeyboard);
-
-})
-
-//method for requesting user's location
-
-bot.hears("location", (ctx) => {
-    console.log(ctx.from)
-    bot.telegram.sendMessage(ctx.chat.id, 'Can we access your location?', requestLocationKeyboard);
-})
-
-//constructor for providing phone number to the bot
-
-const requestPhoneKeyboard = {
-    "reply_markup": {
-        "one_time_keyboard": true,
-        "keyboard": [
-            [{
-                text: "My phone number",
-                request_contact: true,
-                one_time_keyboard: true
-            }],
-            ["Cancel"]
-        ]
-    }
-};
-//constructor for proving location to the bot
-
-const requestLocationKeyboard = {
-    "reply_markup": {
-        "one_time_keyboard": true,
-        "keyboard": [
-            [{
-                text: "My location",
-                request_location: true,
-                one_time_keyboard: true
-            }],
-            ["Cancel"]
-        ]
-    }
-
-}
-
-bot.launch();
+bot.launch()
---
+++ menu.js
@@ -0,0 +1,3 @@
+// export interface MyContext extends TelegrafContext {
+// 	readonly match: RegExpExecArray | undefined;
+// }
